d68cda3d3dc21ef2fcfca54033bab1d403643622
DuongXuanHoangVu <hoangvu.k38@gmail.com>
2022-12-30 21:38:44 +0700

    Dựng khung Layout mặc định

--- src/components/Layouts/DefaultLayout/SideBar/index.js
+++ src/components/Layouts/DefaultLayout/SideBar/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './SideBar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function SideBar() {
-    return <h2>SideBar</h2>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Side bar</h2>
+        </aside>
+    );
 }
 
 export default SideBar;
--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,13 +1,17 @@
+import classNames from 'classnames/bind';
 import Header from '~/components/Layouts/components/Header';
+import styles from './DefaultLayout.module.scss';
 import SideBar from './SideBar';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <SideBar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
